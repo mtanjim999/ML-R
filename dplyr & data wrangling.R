eb6e06eb9827fb5c40f77f57d65a08956f3d2cb5
Tanjim Ahamed <m.tanjim999@gmail.com>
2025-08-02 03:02:29 +0600

    Update dplyr & data wrangling.R

--- dplyr & data wrangling.R
+++ dplyr & data wrangling.R
@@ -45,3 +45,28 @@ gapminder %>%
   arrange(totalgdp) %>%  #lower value to higher
   arrange(desc(totalgdp)) # higher value to lower
 
+#use of summary() function
+gapminder %>% 
+  summarise(minimum = min(pop),
+            average = mean(pop),
+            maximum = max(pop),
+            total = n())
+
+
+# use of group_ function
+gapminder %>% 
+  group_by(continent)%>%
+  summarise(minimum = min(pop),
+            average = mean(pop),
+            maximum = max(pop),
+            total = n())
+
+gapminder %>% 
+  group_by(continent, country)%>%
+  summarise(minimum = min(pop),
+            average = mean(pop),
+            maximum = max(pop),
+            total = n())
+
+
+
